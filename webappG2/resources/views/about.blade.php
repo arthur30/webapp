532e73d40f41c9b00a9eddc9555019a982e7d31b
Ruxandra <rn1815@ic.ac.uk>
2017-06-07 19:11:08 +0100

    changed about page

--- webappG2/resources/views/about.blade.php
+++ webappG2/resources/views/about.blade.php
@@ -1,19 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>
-            <p> <b>GoLocal</b> is a client-server software application, built up using Laravel, a PHP framework for web artisans.
-                It is addressed to a wide variety of people, who either want to promote their city or want to discover
-                the most beautiful places in this world, through the eyes of an inhabitant. It provides all the facilities
-                for those who want to explore, discover and wonder.</p>
 
-            <br><br>
 
-            <p><b>GoLocal</b> is a new concept, perfect for everyone who aims to travel through the values, the authenticity
-                and the tradition of a peculiar place. It broadens the horizon of every travelling lover, giving the tourist
-                this amazing opportunity of meeting the most suitable residents of the city, who will surround him with the local
-                scent of the place. It gives you a different vision, changes your perspective and introduces you to an unknown beauty.</p>
-        </h1>
-    </div>
+<body style="background-color: #F5F5DC; font-size: large">
+  <div class="container">
+    <h1>
+      <h3> <b>What to expect?</b></h3>
+       <p>  GoLocal facilitates your desire to see the world through someone else's eyes. It does this by connecting you
+            with a new kind of guides. They are people who live in the cities you are visiting and know it as the back of their hand.
+            They are here to offer you an authentic experience, to help you distance yourself from the standard idea of a tourist
+            and allow you immerse yourself into the mistery and excitement of a new place. They help you wander off the
+            beaten path, they help you <b>GoLocal</b>.</p>
+     <h3><b>What does a guide do?</b> </h3>
+       <p>
+         <ul style="list-style: square">
+          <li>Helps you avoid <b style="font-size: 200%">tourist traps</b>
+            <ul>  <li style="list-style: disc"> overpriced/overrated/non-authentic places</li> </ul>
+          <li>Helps you have a <b style="font-size: 260%">safe</b> trip</li>
+            <ul>  <li style="list-style: disc"> points out places where you can get tricked or mugged</li> </ul>
+          <li> Helps you with <b style="font-size: 220%">organization</b></li>
+            <ul>  <li style="list-style: disc"> tells you the best way and time to get to a place: avoid traffic jams and queues </li> </ul>
+          <li>Helps you get an  <b style="font-size: 270%">authentic</b> experience </li>
+            <ul>  <li style="list-style: disc"> recommends local cuisine and tells you about customs</li> </ul>
+            <ul>  <li style="list-style: disc"> shows you places where locals go to eat, relax, drink etc. </li> </ul>
+          <li> May provide  <b style="font-size: 160%">optional </b> services </li>
+            <ul>  <li style="list-style: disc"> accompany you on your trip if requested </li> </ul>
+        </ul>
+       </p>
+   </h1>
+
+  </div>
+</body>
+
 @endsection
