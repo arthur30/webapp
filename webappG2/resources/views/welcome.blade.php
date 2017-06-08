2a630fd03f205a1179832eb3b953f3223c1270d6
ab6015 <andrada-ioana.bogoi15@imperial.ac.uk>
2017-06-08 16:12:05 +0100

    Started Search button

--- webappG2/resources/views/welcome.blade.php
+++ webappG2/resources/views/welcome.blade.php
@@ -117,7 +117,8 @@
                     </div>
                 </div>
             </div>
-            <p><button class="w3-button w3-dark-grey">Search</button></p>
+            <p><button class="w3-button w3-dark-grey" method="POST" action="{{ route('location.submit') }}">Search</button></p>
+
         </div>
 
         <div id="Flight" class="w3-container w3-white w3-padding-16 myLink">
