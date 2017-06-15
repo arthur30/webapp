c03ae26f3fbd7f1b18b7ffae63c5f48496fc79b2
arthur30 <arthur.niculae96@gmail.com>
2017-06-15 20:12:17 +0100

    Implemented a ContacUser model and created corresponding table in database

---
+++ app/ContactUser.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+
+class ContactUser extends Model
+{
+    /**
+     * ContactUser constructor for post which specifies that the user must be logged in to message guide
+     * Both tourist and guides can have access at it
+     */
+    public function __construct()
+    {
+        $this->middleware('auth', 'auth:guide');
+    }
+
+    // Return the view to contact a user
+    public function create()
+    {
+        $user = (Auth::guard('guide')) ? 'guide' : 'tourist';
+        return view($user . 's.message-' . $user);
+    }
+
+    // Store the user message details in the database
+    public function store(Request $request)
+    {
+        $user = (Auth::guard('guide')) ? 'guide' : 'tourist';
+
+        $this->validate(request(), [
+            'tourist_id' => 'required',
+            'guide_id' => 'required',
+            'description' => 'required|string|max:500',
+            'message' => 'required|string|min:1',
+        ]);
+
+        // Create a new message instance for the user after a valid form submission
+        ContactUser::create([
+            'tourist_id' => $request['tourist_id'],
+            'guide_id' => $request['guide_id'],
+            'description' => $request['description'],
+            'message' => $request['message'],
+        ]);
+
+        // Redirect the user to the dashboard after submitting a message
+        return redirect(route($user . '.dashboard'));
+    }
+}
---
+++ resources/views/guides/message-tourist.blade.php
@@ -0,0 +1,12 @@
+<!-- Message a tourist -->
+@extends('layouts.app')
+
+@section('content')
+    <div class="w3-content" style="max-width:1100px;">
+        <div class="w3-container">
+            <div class="w3-row-padding">
+                <h1 style="text-align: center"> Message tourist </h1>
+            </div>
+        </div>
+    </div>
+@endsection
