e1731ba90c9b38ef9c1cc92cfce2f29736a6da04
arthur30 <arthur.niculae96@gmail.com>
2017-06-15 13:10:06 +0100

    Implemented a partial guide personal page

--- resources/views/guides/guide-personal-page.blade.php
+++ resources/views/guides/guide-personal-page.blade.php
@@ -2,16 +2,27 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">A guide page</div>
-
-                    <div class="panel-body">
-                        Here will be the page of your guide
-                        {{ $guide->name }}
-                    </div>
+    <!-- Guide personal page content -->
+    <div class="w3-content" style="max-width:1100px;">
+        <div class="w3-container">
+            <div class="w3-row-padding">
+                <h1 style="text-align: center"> {{ $guide->name }}'s Profile </h1><br>
+                <div class="w3-third">
+                    <img src="/uploads/backgrounds/LasVegas.jpg" alt="LasVegas" style="width:100%">
+                </div>
+                <div class="w3-third">
+                    <h3><b> Description </b></h3>
+                    <p> {{ $guide->description }} </p>
+                </div>
+                <div class="w3-third">
+                    <h3><b> Languages </b></h3>
+                    <p> English </p>
+                    <h3><b> Top Preferences </b></h3>
+                    <p> Hyde Park </p>
+                    <p> National History Museum </p>
+                    <a href={{ route('about') }}>
+                        <button class="w3-button w3-margin-bottom w3-grey">Message {{ $guide->first_name }}</button>
+                    </a>
                 </div>
             </div>
         </div>
